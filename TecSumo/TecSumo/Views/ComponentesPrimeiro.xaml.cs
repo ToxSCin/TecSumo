c41f3dd9597d0d12da222a6cbd9a97563b663d2c
Usuario <Usuario@CPD03-M13>
2022-10-05 15:26:07 -0300

    Adicionar arquivos de projeto.

---
+++ TecSumo/TecSumo/Views/ComponentesPrimeiro.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TecSumo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ComponentesPrimeiro : ContentPage
+    {
+        public ComponentesPrimeiro()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_5(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_6(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_7(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
